f62feafa6089b6c33f40c1599132ba258b42797d
lblack <wanglongqi77@gmail.com>
2021-04-30 19:23:07 -0400

    fixed REST protocols

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,13 +14,13 @@ router.get("/", function (req, res) {
     });
 });
 
-router.post("/api/burgers", function (req, res) {
+router.post("/api/burger", function (req, res) {
     burger.insertOne("burger_name", req.body.burger_name, function (result) {
         res.json({ id: result.insertId });
     });
 });
 
-router.put("/api/burgers/:id", function (req, res) {
+router.put("/api/burger/:id", function (req, res) {
     const id = req.params.id
     burger.updateOne("burgers","devoured", req.body.devoured, id, function (result) {
         if (result.changedRows == 0) {
